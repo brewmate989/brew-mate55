aeb1921750ae950dcb0e53a75c12d511339fb682
brewmate989 <brewmate989@gmail.com>
2026-06-11 08:26:54 +0700

    fix: base path brew-mate.github.io

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/brew-mate55/",
+  base: "/brew-mate.github.io/",
   build: {
     outDir: "docs",
   },
